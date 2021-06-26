384a10f9031229dc21f59adc931888c131949631
Ahmed Halac <38500014+ahmedhalac@users.noreply.github.com>
2021-06-26 22:15:33 +0200

    skeleton_finished

---
+++ app/src/main/java/com/example/androidwebapi/screens/datagroup/DataGroupFragment.kt
@@ -0,0 +1,37 @@
+package com.example.androidwebapi.screens.datagroup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.androidwebapi.R
+import com.example.androidwebapi.databinding.FragmentDataGroupBinding
+import com.example.androidwebapi.databinding.FragmentIntroBinding
+
+class DataGroupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentDataGroupBinding>(
+            inflater,
+            R.layout.fragment_data_group,
+            container,
+            false
+        )
+
+
+        binding.popularMoviesBtn.setOnClickListener{view: View ->
+            view.findNavController().navigate(R.id.action_dataGroupFragment_to_popularMoviesFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
