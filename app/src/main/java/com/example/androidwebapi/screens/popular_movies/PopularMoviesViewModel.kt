817ea0e2e9cae689e960237ee0dc29a8787459d7
Ahmed Halac <38500014+ahmedhalac@users.noreply.github.com>
2021-06-28 16:38:13 +0200

    displaying-internet-img

--- app/src/main/java/com/example/androidwebapi/screens/popular_movies/PopularMoviesViewModel.kt
+++ app/src/main/java/com/example/androidwebapi/screens/popular_movies/PopularMoviesViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.androidwebapi.screens.popular_movies
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.androidwebapi.network.PopularMovies
 import com.example.androidwebapi.network.PopularMoviesApi
 import kotlinx.coroutines.launch
 
@@ -18,11 +18,20 @@ class PopularMoviesViewModel : ViewModel() {
         getPopularMovies()
     }
 
+    private val _property = MutableLiveData<PopularMovies.Result>()
+
+    val property: LiveData<PopularMovies.Result>
+        get() = _property
+
     private fun getPopularMovies() {
         viewModelScope.launch {
             try {
                 val listResult = PopularMoviesApi.retrofitService.getProperties()
                 _response.value = "Success: ${listResult} Movies properties retrieved"
+                if (listResult.results.size > 0) {
+                    _property.value = listResult.results[0]
+                }
+
             }catch(e : Exception) {
                 _response.value = "Failure: ${e.message} "
             }
