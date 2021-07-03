b6c78c16fbd3f3e17ebc87760843a239dfed0cb0
Ahmed Halac <38500014+ahmedhalac@users.noreply.github.com>
2021-07-04 01:21:20 +0200

    movies-filtering

--- app/src/main/java/com/example/androidwebapi/network/MoviesApiService.kt
+++ app/src/main/java/com/example/androidwebapi/network/MoviesApiService.kt
@@ -2,14 +2,27 @@ package com.example.androidwebapi.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 
-private const val API_KEY = "?api_key=b2a4448503d46709ed8c8e90ec1bf7af"
 private const val BASE_URL = "https://api.themoviedb.org/3/movie/"
 
+enum class MovieApiFilter(val value: String) {
+    SHOW_UPCOMING("upcoming"),
+    SHOW_TOP_RATED("top_rated"),
+    SHOW_POPULAR("popular") }
+
+//Logger
+private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+private var okHttp = OkHttpClient.Builder().addInterceptor(logger)
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -18,12 +31,14 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(okHttp.build())
     .build()
 
 
 interface MoviesApiService {
-    @GET("popular$API_KEY")
-    suspend fun getProperties(): Movies
+    @GET("{movie_type}")
+    suspend fun getProperties(@Path(value = "movie_type", encoded = true) movieId : String,
+    @Query("api_key", encoded = true) api_key : String): Movies
 }
 
 
