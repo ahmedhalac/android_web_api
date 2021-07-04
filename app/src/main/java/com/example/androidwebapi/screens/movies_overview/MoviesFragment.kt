996498dc82e8c531059ee3f7f5ec55e0cc021e2e
Ahmed Halac <38500014+ahmedhalac@users.noreply.github.com>
2021-07-04 22:31:07 +0200

    final-commit

--- app/src/main/java/com/example/androidwebapi/screens/movies_overview/MoviesFragment.kt
+++ app/src/main/java/com/example/androidwebapi/screens/movies_overview/MoviesFragment.kt
@@ -18,7 +18,7 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val binding = FragmentMoviesBinding.inflate(inflater)
 
